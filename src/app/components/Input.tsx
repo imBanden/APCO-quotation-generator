77f4138dbad3f3f5ed4dea2188e0b93698eef7fa
imBanden <brandon@majilabs.com>
2024-12-12 16:35:16 +0800

    temporary fixed InputValue

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -17,7 +17,7 @@ const Input = ({
 }: InputProps) => {
   const [isFocused, setIsFocused] = useState<boolean>(false);
   const { formData, setFormData } = useFormContext();
-  const [inputValue, setInputValue] = useState(formData[keyValue]);
+  // const [inputValue, setInputValue] = useState(formData[keyValue]);
 
   return (
     <div className="flex flex-col">
@@ -31,13 +31,13 @@ const Input = ({
       <input
         className="border border-slate-400 outline-none text-slate-950 px-2 py-1 text-xs focus:border-slate-950 placeholder:text-xs transition-all duration-300"
         placeholder={placeholder}
-        value={inputValue}
+        // value={inputValue}
         type={inputType}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
         onChange={(e) => {
           const newInputValue = e.target.value;
-          setInputValue(newInputValue);
+          // setInputValue(newInputValue);
           setFormData((prev) => ({ ...prev, [keyValue]: newInputValue }));
         }}
       ></input>
